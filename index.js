1c52c7a513c3165e5254b063d8873b8fd68447fb
Charles Pletcher <cpletcher2@gmail.com>
2016-06-14 22:34:38 -0400

    Add solution

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  return n += 1
+}
+
+function dec(n) {
+  return n -= 1
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
