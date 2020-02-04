54489bde7affdc87ecc7d4a5ad43a87416d9832a
maxwellbenton <maxwellbenton@gmail.com>
2020-02-03 19:30:20 -0500

    update solution

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-function add(a, b) {
+function add(a,b) {
   return a + b
 }
 
-function subtract(a, b) {
+function subtract(a,b) {
   return a - b
 }
 
-function multiply(a, b) {
-  return a * b
-}
-
-function divide(a, b) {
+function divide(a,b) {
   return a / b
 }
 
-function inc(n) {
+function multiply(a,b) {
+  return a * b
+}
+
+function increment(n) {
   return n += 1
 }
 
-function dec(n) {
+function decrement(n) {
   return n -= 1
 }
 
-function makeInt(n) {
-  return parseInt(n, 10)
+function makeInt(string) {
+  return parseInt(string, 10)
 }
 
-function preserveDecimal(n) {
-  return parseFloat(n)
+function preserveDecimal(string) {
+  return parseFloat(string)
 }
