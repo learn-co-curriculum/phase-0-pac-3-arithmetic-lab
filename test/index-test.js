0c54e1f583c1e74b6948a4db04da82857b8d4a65
Liz Burton <liz.burton147@gmail.com>
2020-07-25 18:19:32 -0400

    Fixes errors in spec file

--- test/index-test.js
+++ test/index-test.js
@@ -11,15 +11,15 @@ describe('basic math functions', function () {
   })
 
   it("'subtract()' is a valid function", function() {
-    expect(add).toExist
+    expect(subtract).toExist
   })
 
-  it("'multiple()' is a valid function", function() {
-    expect(add).toExist
+  it("'multiply()' is a valid function", function() {
+    expect(multiply).toExist
   })
 
   it("'divide()' is a valid function", function() {
-    expect(add).toExist
+    expect(divide).toExist
   })
 
   it('add(a, b) adds two numbers and returns the result', function() {
