4b5182f77246f6525147a06d1a34f664fb0f6d09
Haala <haalahadithy@gmail.com>
2022-05-25 15:00:06 -0700

    commit tree 1b3b2789d40375e77c6c90286b7ee4382f2a6908

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a , b){
+    return (a+b);
+}
+
+function subtract(a , b){
+    return (a-b);
+}
+
+function multiply(a , b){
+    return (a*b);
+}
+
+function divide(a, b){
+    return (a/b);
+}
+
+function increment(n){
+    return (n += 1);               //can do (n += 1) OR (++n)
+}
+
+function decrement(n){
+    return (n -= 1);                  //can do (n -= 1) OR (--n)
+}
+
+function makeInt(n){
+    return (parseFloat(n));  
+}
+
+function preserveDecimal(n){
+    return (parseFloat(n));
+}
+
