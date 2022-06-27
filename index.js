c35b2b9e146544040cdbf01de923d7fa969864b4
Roger Hicks <txleadfoot00@yahoo.com>
2022-06-26 18:10:36 -0700

    commit tree bf253ec9528cf804bed71a36a55e93c8631cf6b9

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  function increment(a) {
+    return (a += 1);
+  }
+  
+  function decrement(a) {
+    return (a -= 1);
+  }
+  
+  function makeInt(n) {
+    return parseInt(n,10);
+  }
+  
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  
