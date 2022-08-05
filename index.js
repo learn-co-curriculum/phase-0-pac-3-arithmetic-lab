5a2c0bdba58f9ba36eebf23b1b41486bc938ee7d
peter <+>
2022-08-04 23:34:21 -0400

    commit tree f518503c7b2a4269bb1f4a8f1f97a1e035bd8e34

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(num1,num2){
+   return num1 + num2 ;
+
+}
+
+function subtract(num1,num2){
+    return num1 - num2;
+}
+
+function multiply(num1,num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+function increment (n){
+    n = n +1;
+    return n;
+}
+
+function decrement (n){
+    n = n -1;
+    return n;
+}
+
+function makeInt(n){
+   return n = parseInt(n,10);
+}
+
+function preserveDecimal(n){
+    return n = parseFloat(n);
+}
