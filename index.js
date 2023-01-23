611fd962de827b6c97118807361050a696d37388
drake bishop <drakemx85@gmail.com>
2023-01-22 20:47:43 -0600

    commit tree 563d15adf3d01190514ad2b31b975d2604092cc2

---
+++ index.js
@@ -0,0 +1,47 @@
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    return ++ n;
+}
+
+function decrement(n) {
+    return -- n;
+}
+
+function makeInt(n) {
+    return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n, 10)
+}
