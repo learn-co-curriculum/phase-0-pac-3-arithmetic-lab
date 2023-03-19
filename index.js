f4ff90b8a403882425a255ee6edb9675315345b5
Bethany Albright <balbright0186@gmail.com>
2023-03-19 08:03:24 -0400

    Adds myfile.md

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a,b) {
+    return a + b;
+ }
+
+  function subtract(a,b) {
+    return a - b;
+ }
+
+  function multiply(a,b) {
+    return a * b;
+}
+
+  function divide(a,b) {
+    return a / b;
+}
+function increment(n) {}
+function increment(n) {
+    return (n+1)
+}
+ function decrement(n) {}
+ function decrement(n) {
+    return (n-1)
+ }
+
+function makeInt(n) {
+    return parseInt("n", 10)
+}
+
+function preserveDecimal(n) {
+    return parseFloat("2.222", 10)
+}
+
+function makeInt(string) {
+    return parseInt(string, 10)
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string, 10)
+}
