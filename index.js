26d55dcb30081b318c15c545b1a3245872d3aae2
Liam Kirkland <liam.kirkland@liam.kirkland>
2026-02-18 13:44:31 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(x, y){
+    return x + y
+}
+
+function subtract(x, y){
+    return x - y
+}
+
+function multiply(x, y){
+    return x * y
+}
+
+function divide(x, y){
+    return x / y
+}
+
+function increment(x){
+    return ++x
+}
+
+function decrement(x){
+    return --x
+}
+
+function makeInt(x){
+    return parseInt(x, 10)
+}
+
+function preserveDecimal(x){
+    return parseFloat(x)
+}
