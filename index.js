203549d558b0f358340fd8e6acad7f7a99a88f71
Tony Tran <tony.tran@powerhrg.com>
2026-03-05 14:13:25 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function increment(n) {
+  return n + 1;
+}
+
+function decrement(n) {
+  return n - 1;
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
