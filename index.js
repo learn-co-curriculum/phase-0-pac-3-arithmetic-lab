9b5cc5ed53dabfe7906c7033c8f711ebb9788dbc
Thomas Correia <thomas.correiap@gmail.com>
2026-03-07 15:50:34 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+function add() {
+  // we'll fill this in shortly
+}
+function subtract() {
+  // we'll fill this in shortly
+}
+function multiply() {
+  // we'll fill this in shortly
+}
+function divide() {
+  // we'll fill this in shortly
+}
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+function increment(n) {
+  return n + 1;
+}
+function decrement(n) {
+  return n - 1;
+}
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
